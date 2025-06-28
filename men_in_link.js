9201a4de50673a03af5b60ae1e73ac056337c3fa
Crewlobbymadgaon <combinedcrewlobbymadgaon@gmail.com>
2025-06-28 22:03:16 +0530

    Add files via upload

---
+++ men_in_link.js
@@ -0,0 +1,16 @@
+const crewLinkMen = {
+  MAO: {
+    "Link 1": 18,
+    "Link 2": 15,
+    "Link 3": 10,
+    "Link 4": 17
+  },
+  RN: {
+    "Link 1": 14,
+    "Link 2": 17,
+    "Link 3": 15
+  },
+  SL: {
+    "Link 1": 2
+  }
+};
